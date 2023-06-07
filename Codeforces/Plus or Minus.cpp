7b3b5bb007d840a4f1378aa3a99b1b3c7a7ab3b7
Your Name~ <tamimafnan308@gmail.com>
2023-06-07 22:21:32 +0600

    .

---
+++ Codeforces/Plus or Minus.cpp
@@ -0,0 +1,35 @@
+
+
+#include<bits/stdc++.h>
+#define fastio  ios_base::sync_with_stdio(0);cin.tie(NULL);
+#define ll long long
+using namespace std;
+
+
+int main()
+{
+    fastio;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+         int n,m,l;
+         cin>>n>>m>>l;
+         if(n+m==l)
+         {
+             cout<<"+"<<endl;
+         }
+         else
+         {
+             cout<<"-"<<endl;
+         }
+
+
+
+    }
+
+
+    return 0;
+}
+
+
