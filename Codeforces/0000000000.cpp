575a7adab4f0833f5d4d2809d4b6a4d2e17e64f5
Tamim148 <tamimafnan308@gmail.com>
2023-08-17 23:15:39 +0600

    .

---
+++ Codeforces/0000000000.cpp
@@ -0,0 +1,40 @@
+
+
+
+#include<bits/stdc++.h>
+#define fastio  ios_base::sync_with_stdio(0);cin.tie(NULL);
+#define ll long long
+using namespace std;
+
+
+int main()
+{
+    fastio;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++)
+        {
+
+        }
+        int flag=1;
+
+
+        if(flag)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+
+
+    return 0;
+}
+
+
